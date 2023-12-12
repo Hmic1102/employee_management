d4c4bd21d4e5bc6b5be845dc6d572f3564196d83
Zhengyang Han <113384319+Hmic1102@users.noreply.github.com>
2023-12-11 19:47:20 -0500

    Delete employee-management directory

--- employee-management/src/main/java/com/hmic/service/EmpService.java
+++
@@ -1,4 +0,0 @@
-package com.hmic.service;
-
-public interface EmpService {
-}
--- employee-management/src/main/java/com/hmic/service/impl/EmpServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.hmic.service.impl;
-
-import com.hmic.service.EmpService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmpServiceImpl implements EmpService {
-}
